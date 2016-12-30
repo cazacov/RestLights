f8635dba9e8c3f18d34b7c00bb2b4f331479fb49
unknown <E:\Mail>
2016-12-30 19:38:18 +0100

    Switch to ONOFF library

--- README.md
+++ README.md
@@ -5,9 +5,40 @@ Light Organ with REST interface
 
 Start with the latest version of Raspbian (tested with ver. 2016-11-25)
 
-Check if Node.js is included
+### Update packages
+
 ```
-node --version
+sudo apt-get update
+sudo apt-get upgrade
 ```
 
-If Node.js is not there, follow this [guide](http://raspberryalphaomega.org.uk/2014/06/11/installing-and-using-node-js-on-raspberry-pi/ "nstalling and using node.js on Raspberry Pi").
+### Install Node.js and npm
+
+#### Raspberry Pi A/A+, B/B+ (ARM v6)
+
+```
+sudo su -
+cd /opt
+wget http://nodejs.org/dist/latest-v6.x/node-v6.9.2-linux-armv6l.tar.gz
+tar -xvf node-v6.9.2-linux-armv6l.tar.gz
+ln -s node-v6.9.2-linux-armv6l node
+chmod a+rw /opt/node/lib/node_modules
+chmod a+rw /opt/node/bin
+echo 'PATH=$PATH:/opt/node/bin' > /etc/profile.d/node.sh
+rm /usr/bin/node
+ln -s  /opt/node/bin/node /usr/bin/node
+^D
+```
+
+#### Raspberry 2,3
+TBD
+
+
+Logout and log in back.  
+
+Check if Node is up-to-date: 
+
+```
+node --version
+npm --version
+```
--- sources/restlights.js
+++ sources/restlights.js
@@ -1,39 +1,8 @@
 var blinkDuration = 10000;
 
-var lamps = [
-    {
-        pin: 12,
-        color: "green",
-        counter: 0
-    },
-    {
-        pin: 18,
-        color: "yellow",
-        counter: 0
-    },
-    {
-        pin: 16,
-        color: "blue",
-        counter: 0
-    },
-    {
-        pin: 15,
-        color: "blue2",
-        counter: 0
-    },
-    {
-        pin: 7,
-        color: "red",
-        counter: 0
-    },
-    {
-        pin: 13,
-        color: "orange",
-        counter: 0
-    }
-];
+var lamps = require('./config/lamps.json');
 
-var gpio = require("pi-gpio");
+var Gpio = require("onoff").Gpio;
 
 console.log("Starting web server...");
 var express = require('express')
@@ -43,8 +12,6 @@ var express = require('express')
 console.log(__dirname);
 
 app.use(cors());
-//app.engine('html', require('ejs').renderFile);
-//app.set('view engine', 'html');
 app.set('view engine', 'jade');
 
 app.use('/css', express.static(__dirname + '/css/'));
@@ -53,21 +20,16 @@ app.use('/app', express.static(__dirname + '/app/'));
 
 app.get('/', function (req, res) {
     res.render('index', { model: lamps });
-    //res.render('index');
 });
 
 app.get('/blink/:color', function (req, res) {
     console.log("POST " + req.params.color);
-
-    // Find the lamp and increment counter
-
     incrementCounter(req.params.color);
     res.render('index', { model: lamps });
 });
 
 openPorts();
 test();
-
 setInterval(updateLamps, 500);
 
 function incrementCounter(color)
@@ -86,112 +48,78 @@ function updateLamps()
     lamps.forEach(function(lamp){
         if (lamp.counter > 0)
         {
-            gpio.read(lamp.pin, function(err, value)
+            var value = lamp.gpio.readSync();
+            if (!value)
             {
-                if (err)
-                {
-                    console.log("Cannot read pin");
-                }
-                else if (!value)
-                {
-                    lamp.counter--;
-                    console.log("Blinking lamp " + lamp.color + ": " + lamp.counter);
-                    blink(lamp.pin, blinkDuration);
-                }
-            });
+                lamp.counter--;
+                console.log("Blinking lamp " + lamp.color + ": " + lamp.counter);
+                blink(lamp.color, blinkDuration);
+            }
         }
     });
 }
 
-function blink(pin, duration)
+function blink(color, duration)
 {
-    gpio.write(pin, 1, function(err) {
-        if (err)
-        {
-            console.log("Write 1 failed: " + err);
-        }
-        setTimeout(function() {
-            gpio.write(pin, 0, function(err) {
-                if (err) {
-                    console.log("Write 0 failed: " + err);
-                }
+    var lamp = lamps.find(function filter(elem) { return elem.color === color;});
 
-            });
+    if (lamp)
+    {
+        lamp.gpio.writeSync(1);
+        setTimeout(function() {
+            lamp.gpio.write(0);
         }, duration);
-    });
+    }
 }
 
 function openPorts()
 {
     // Initialize Raspberry GPIO
     lamps.forEach(function(lamp){
-        gpio.open(lamp.pin, "output", function(err) {
-            if (err)
-            {
-                console.log(err)
-            }
-            else {
-                gpio.write(lamp.pin, 0);
-            }
-        });
+        console.log("Opening GPIO pin " + lamp.pin);
+        lamp.gpio = new Gpio(lamp.pin, 'out');
     });
 
-    gpio.open(22, "output", function(err) {
-        gpio.write(22, 1);
-    });
+    var led = new Gpio(25, 'out');
+    led.writeSync(1);
 }
 
-function closePorts()
+function powerOff()
 {
-    gpio.write(22, 0);
-    lamps.forEach(function(lamp){
-        try {
-            console.log("CLosing pin " + lamp.pin);
-            gpio.close(lamp.pin, function(err) {
-                console.log("Pin " + lap.pin + " closed " + err);
-            });
-        }
-        catch (err){
-            console.log("Pin " + lap.pin + " closed with an error " + err);
-        }
-    });
-    gpio.close(22, function(err) {
-        console.log("Pin 22 closed " + err);
-    });
+    var led = new Gpio(25, 'out');      // Pin 22
+    led.writeSync(0);
+    console.log('bye');
 }
 
 function test() {
-    setTimeout(function() {blink(16, 3000);}, 1000);
-    setTimeout(function() {blink(18, 3000);}, 1500);
-    setTimeout(function() {blink(7, 3000);}, 2000);
-    setTimeout(function() {blink(13, 3000);}, 2500);
-    setTimeout(function() {blink(15, 3000);}, 3000);
-    setTimeout(function() {blink(12, 3000);}, 3500);
+    setTimeout(function() {blink('green', 3000);}, 1000);
+    setTimeout(function() {blink('blue', 3000);}, 1500);
+    setTimeout(function() {blink('yellow', 3000);}, 2000);
+    setTimeout(function() {blink('red', 3000);}, 2500);
+    setTimeout(function() {blink('orange', 3000);}, 3000);
+    setTimeout(function() {blink('blue2', 3000);}, 3500);
 }
 
 process.on('exit', function () {
-    console.log('Exiting ...');
-    closePorts();
-    // close other resources here
-    console.log('bye');
+    powerOff();
 });
 
 // happens when you press Ctrl+C
 process.on('SIGINT', function () {
     console.log( '\nGracefully shutting down from  SIGINT (Crtl-C)' );
-    closePorts();
+    powerOff();
     process.exit();
 });
 
 // usually called with kill
 process.on('SIGTERM', function () {
     console.log('Parent SIGTERM detected (kill)');
-    closePorts();
+    powerOff();
     process.exit(0);
 });
 
 
-var port = 80;
+var port = 3000;
 app.listen(port, function() {
     console.log('Server listening at %s', port);
 });
